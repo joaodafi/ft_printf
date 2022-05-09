42d450a0badf4ecad7cd1800a8d8e0c4e79dd4ee
jomiguel <joao.miguel@hotmail.com>
2022-05-09 17:25:31 +0000

    fix typo ft_printf file name

--- ft_printf.h
+++ ft_printf.h
@@ -6,15 +6,15 @@
 /*   By: jomiguel < jomiguel@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 12:19:10 by jomiguel          #+#    #+#             */
-/*   Updated: 2022/05/09 15:52:27 by jomiguel         ###   ########.fr       */
+/*   Updated: 2022/05/09 17:23:25 by jomiguel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include "libft/libft.h"
 # include <stdarg.h>
+# include "libft/libft.h"
 
 # define DECIMAL_BASE "0123456789"
 # define HEXADECIMAL_BASE "0123456789abcdef"
